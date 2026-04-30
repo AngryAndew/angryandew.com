1fb0c7cb37f98b24c7d1bdf89153bc9651d22f0d
Steven <sebradley13+github2@gmail.com>
2026-04-29 17:01:11 -0700

    Wire App.js to use useColorMode hook and getTheme for dark mode support

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
+import { getTheme } from './theme';
+import { useColorMode } from './useColorMode';
 import LandingPage from './components/LandingPage';
 
 function App() {
+  const { mode, toggleColorMode } = useColorMode();
+  const theme = useMemo(() => getTheme(mode), [mode]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <LandingPage />
+      <LandingPage mode={mode} onToggleColorMode={toggleColorMode} />
     </ThemeProvider>
   );
 }
